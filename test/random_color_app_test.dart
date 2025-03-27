127446b8dd02bcaf72b6af8646878f5205a1b24a
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2025-03-27 13:49:15 +0200

    test: add tests for RandomColorApp

---
+++ test/random_color_app_test.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:random_color_app/core/theme_notifier.dart';
+import 'package:random_color_app/home_screen.dart';
+import 'package:random_color_app/random_color_app.dart';
+
+void main() {
+  group('Random Color App', () {
+    testWidgets('Shows HomeScreen', (tester) async {
+      await tester.pumpWidget(const ProviderScope(child: RandomColorApp()));
+
+      expect(find.byType(HomeScreen), findsOneWidget);
+    });
+
+    testWidgets('Changes MaterialApp theme on ThemeNotifier change', (
+      tester,
+    ) async {
+      final providerContainer = ProviderContainer();
+      addTearDown(providerContainer.dispose);
+
+      final themeNotifier = providerContainer.read(themeNotifierProvider);
+
+      await tester.pumpWidget(
+        UncontrolledProviderScope(
+          container: providerContainer,
+          child: const RandomColorApp(),
+        ),
+      );
+
+      MaterialApp materialApp = tester.widget<MaterialApp>(
+        find.byType(MaterialApp),
+      );
+
+      expect(materialApp.theme, themeNotifier.currentTheme);
+
+      themeNotifier.currentTheme = ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+      );
+
+      await tester.pumpAndSettle();
+
+      materialApp = tester.widget<MaterialApp>(find.byType(MaterialApp));
+
+      expect(materialApp.theme, themeNotifier.currentTheme);
+    });
+  });
+}
